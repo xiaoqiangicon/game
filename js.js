1481daa0122e9e413a241dd295cdeac0e8505d60
lee <563165732@qq.com>
2017-07-24 21:29:47 +0800

    finnal

--- js.js
+++ js.js
@@ -1,8 +1,9 @@
 window.onload=function(){
 	var timer=null;
+	var oScore=document.getElementById('score');
 	var oMain=document.getElementById('main');
 	var oContainer=getByClass(oMain,'container')[0];
-	var iSpeed=1;
+	var iSpeed=3;
 	
 	timer=setInterval(function(){
 		var iTop=parseInt(getStyle(oContainer,'top'));
@@ -14,13 +15,27 @@ window.onload=function(){
 		}
 		oContainer.style.top=iTop+'px';
 
-		if(iTop>=0){
+		if(iTop==0){
+			var aRows = oContainer.childNodes;
 			createRow();
 			oContainer.style.top=-120+'px';
 			drow();
+			if( (aRows.length == 5) && (aRows[aRows.length-1].pass !== 1)) {
+				fail();
+			} 
 		}
 		
 	},30);
+
+	oMain.onclick=function(ev){
+		if(ev.target.className.indexOf('black')!=-1){
+			ev.target.className='block';
+			ev.target.parentNode.pass = 1;
+			score();
+		}else{
+			fail();
+		}
+	}
 
 	function init(){
 		for(var i=0;i<4;i++){
@@ -28,6 +43,23 @@ window.onload=function(){
 		}
 	}
 
+	//失败
+	function fail(){
+		clearInterval(timer);
+		alert(oScore.innerHTML)
+	}
+
+	//得分
+	function score(){
+		var newScore=parseInt(oScore.innerHTML)+1;
+		oScore.innerHTML=newScore;
+
+		if(newScore%10==0){
+			iSpeed++;
+		}
+	}
+
+	//删除最后一行
 	function drow(){
 		var oContainer=getByClass(oMain,'container')[0];
 		if(oContainer.childNodes.length == 6) {
@@ -35,6 +67,7 @@ window.onload=function(){
 		}
 	}
 
+	//生成一行
 	function createRow(){
 		var oRow=document.createElement('div');
 		oRow.className='row';
@@ -45,8 +78,13 @@ window.onload=function(){
 			oBlock.className=aClass[i];
 			oRow.appendChild(oBlock);
 		}
-		oContainer.appendChild(oRow);
+		if(oContainer.firstChild==null){
+			oContainer.appendChild(oRow);
+		}else{
+			oContainer.insertBefore(oRow , oContainer.firstChild);
+		}
 	}
+
 	init();
 }
 
@@ -60,6 +98,7 @@ function createBlock(){
 	return arr;
 }
 
+//通过class获取元素
 function getByClass(oPar,sclass){
 	var aClass=oPar.getElementsByTagName('*');
 	var iResult=[];
@@ -72,6 +111,7 @@ function getByClass(oPar,sclass){
 	return iResult;
 }
 
+//获取元素的样式
 function getStyle(obj,attr){
 	if(obj.currentStyle)
 		return obj.currentStyle[attr];
